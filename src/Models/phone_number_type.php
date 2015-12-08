895711c17f6e392b98f4818eab0f329594f8c184
Tyler-Gauch <tyler.gauch@continuum.net>
2015-12-08 10:04:04 -0500

    Update phone_number_type.php

--- src/Models/phone_number_type.php
+++ src/Models/phone_number_type.php
@@ -4,9 +4,8 @@ namespace Digi\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class phone_number_type extends Model
+class phone_type extends Model
 {
-	protected $table = "phone_types";
     public function Contacts()
     {
     	return $this->hasMany("Digi\Models\contact", "phone_number_type");
