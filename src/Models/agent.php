409c54b0497a97d6509716b8bb49ea4692aa1f64
Joe Bai <joe@joebai.com>
2015-12-08 10:18:58 -0500

    Update agent.php

--- src/Models/agent.php
+++ src/Models/agent.php
@@ -2,10 +2,41 @@
 
 namespace Digi\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class agent extends Model
+class agent extends Model implements AuthenticatableContract,
+                                    AuthorizableContract,
+                                    CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'agents';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['agent_first_name', 'agent_middle_name', 'agent_last_name', 'email', 'password', 'agency_id'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+     
+    protected $hidden = ['password', 'remember_token'];
     public function Agency()
     {
     	return $this->belongsTo("Digi\Models\agency");
